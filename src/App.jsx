b402be70730afd44320ebb638a48e8ca4a6257dd
mohamed-zaa <towin011@gmail.com>
2026-09-21 09:08:23 +0300

    Add UserCard component

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Greeting ,WelcomeMessage} from "./Greeting";
+import Usercard from "./Custom"
+
+
+function App() {
+  return (
+    
+  <div>
+    <Usercard/>
+  </div>
+
+  );
+}
+
+export default App
---
+++ src/Custom.jsx
@@ -0,0 +1,19 @@
+
+
+
+const  Usercard =()=> {
+
+return ( 
+
+<div>
+
+<h2>john doe</h2>
+<p>john.doe@example.com</p>
+
+</div>
+)
+
+
+
+}
+export default Usercard 
